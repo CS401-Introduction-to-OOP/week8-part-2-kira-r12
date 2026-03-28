e4bd27fdf28d1f45eecb329c7ece96a7bf7a407b
Kira <reshetilokira513@gmail.com>
2026-03-28 17:16:48 +0200

    Task3

--- Point.cs
+++
@@ -1,9 +0,0 @@
-public struct Point
-{
-public int X { get; set; }
-public int Y { get; set; }
-public void Print()
-    {
-        Console.WriteLine($"Point coordinates: X = {X}, Y = {Y}");
-    }
-}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,17 @@
-﻿Console.WriteLine("Value type demo:");
-Point p1 = new Point { X = 1, Y = 2 };
-Point p2 = p1;
+﻿var container = new DataContainer<DataItem>();
+for (int i = 0; i < 10000; i++)
+{
+var item = new DataItem
+{
+Id = $"item-{i}",
+Payload = new byte[1024]
+};
 
-p2.X = 200;
+    container.AddItem(item);
+}
+Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+GC.Collect();
 
-Console.Write("p1 -- ");
-p1.Print();
-
-Console.Write("p2 -- ");
-p2.Print();
-
-Console.Write("Reference type demo:");
-PointRef r1 = new PointRef { X = 1, Y = 2 };
-PointRef r2 = r1;
-r2.X = 200;
-
-Console.Write("r1 -- ");
-r1.Print();
-Console.Write("r2 -- ");
-r2.Print();
-
-Console.Write("Boxing demo:");
-BoxingTester.Test();
+Console.WriteLine("GC.Collect() was called.");
+Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
+Console.WriteLine("Пояснення:всі обьєкти, що ми створили лежать у списку і тому розмір памяті не змінився, оскільки видалення відбувається за умови, що на обьєкти ніхто не посилається");
