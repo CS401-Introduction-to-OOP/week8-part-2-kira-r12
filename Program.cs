b7d19b62b7542a512e8873f988e7a4da3fb0af0c
Kira <reshetilokira513@gmail.com>
2026-03-28 17:26:44 +0200

    explanation

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ p1.Print();
 
 Console.Write("p2 -- ");
 p2.Print();
+Console.Write("Пояснення: р1 і р2 незалежні копії у стеці --> р2.X не вплинув на р1.X");
 
 Console.Write("Reference type demo:");
 PointRef r1 = new PointRef { X = 1, Y = 2 };
@@ -19,6 +20,7 @@ Console.Write("r1 -- ");
 r1.Print();
 Console.Write("r2 -- ");
 r2.Print();
+Console.Write("Пояснення: r1 і r2 посилання що вказують на один обьєкт у Heap --> змінивши r2.X змінився і r1.X");
 
 Console.Write("Boxing demo:");
 BoxingTester.Test();
